725829aa1d95152b4cca5d2e8f37b71e97ee0cec
walaa <wlaa21511@gmail.com>
2025-02-21 17:54:54 +0200

    withhashlacation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -11,7 +11,7 @@ import { NgxSpinnerModule } from "ngx-spinner";
 import { loadingInterceptor } from './core/intercaptor/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes,withHashLocation()), provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(),withInterceptors([authInterceptor,loadingInterceptor])),
     importProvidersFrom(BrowserAnimationsModule,NgxSpinnerModule),  provideToastr()
   ]
